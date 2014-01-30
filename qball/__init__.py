eda535314e679f9cde0c5c452f70edb179b6b49c
Matt Ferrante <mferrante@dataxu.com>
2014-01-30 16:35:59 -0500

    Added qball package

---
+++ qball/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["QBall", "QBallException"]
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     author_email='mferrante3@gmail.com',
     description='Python integration for qball',
     license='(c) Copyright 2014. All Rights Reserved.',
+    packages=['qball'],
     install_requires=['httplib2 >= 0.8'],
     setup_requires=['httplib2'],
     version='0.1.0',
